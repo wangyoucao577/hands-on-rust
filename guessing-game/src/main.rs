9928e3fd5bf9e0be0460ef5b5293d6bdda7162db
Jay Zhang <wangyoucao577@gmail.com>
2023-05-27 07:43:16 +0000

    feat: seperate libs

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -2,6 +2,8 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+use guessing_game::Guess;
+
 fn main() {
     println!("Guess the number!");
 
@@ -23,9 +25,9 @@ fn main() {
             Err(_) => continue,
         };
 
-        println!("You guessed: {}", guess.value);
+        println!("You guessed: {}", guess.value());
 
-        match guess.value.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
@@ -33,44 +35,5 @@ fn main() {
                 break;
             }
         }
-    }
-}
-
-pub struct Guess {
-    value: i32,
-}
-
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 {
-            panic!(
-                "Guess value must be greater than or equal to 1, got {}.",
-                value
-            )
-        } else if value > 100 {
-            panic!(
-                "Guess value must be less than or equal to 100, got {}.",
-                value
-            )
-        }
-
-        Guess { value }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    #[should_panic]
-    fn greater_than_100() {
-        Guess::new(200);
-    }
-
-    #[test]
-    #[should_panic(expected = "less than or equal to 100")]
-    fn greater_than_100_with_expect() {
-        Guess::new(200);
     }
 }
