f6d02293efbc3345752baca170fcb9b248f7ec55
Jay Zhang <wangyoucao577@gmail.com>
2023-05-28 01:37:17 +0000

    feat: add search function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,14 +8,14 @@ fn main() {
     // dbg!(args);
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem prasing arguments: {err}");
+        eprintln!("Problem prasing arguments: {err}");
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
