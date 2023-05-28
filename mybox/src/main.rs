b092302aa4a1a68fa89cad7cc113d3a89b3bd2d6
Jay Zhang <wangyoucao577@gmail.com>
2023-05-28 12:39:51 +0000

    feat: mybox

---
+++ mybox/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let z = MyBox::new(x);
+    assert_eq!(5, *z);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
