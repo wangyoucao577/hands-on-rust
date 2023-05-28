da44cf957fb0491f2be57654623f4d1c4365a9c2
Jay Zhang <wangyoucao577@gmail.com>
2023-05-28 12:53:00 +0000

    feat: demostrate drop

---
+++ custom_smart_pointer/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let _d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created!");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
