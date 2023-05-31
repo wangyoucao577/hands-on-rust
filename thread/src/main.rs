186daf8a8e2f3ce7f8f9d537d746d0d3040a16d2
Jay Zhang <wangyoucao577@gmail.com>
2023-05-31 01:44:27 +0000

    feat: channels

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -1,7 +1,22 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
+    println!("---- simple_case ----");
+    simple_case();
+    println!("\n\n");
+
+    println!("---- channel_single_producer ----");
+    channel_single_producer();
+    println!("\n\n");
+
+    println!("---- channel_multiple_producers ----");
+    channel_multiple_producers();
+    println!("\n\n");
+}
+
+fn simple_case() {
     let v = vec![0; 5];
 
     let handle = thread::spawn(move || {
@@ -19,4 +34,68 @@ fn main() {
     }
 
     handle.join().unwrap();
+}
+
+fn channel_single_producer() {
+    let (tx, rx) = mpsc::channel();
+
+    let handle = thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for r in rx {
+        println!("Got {}", r);
+    }
+
+    handle.join().unwrap();
+}
+
+fn channel_multiple_producers() {
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+
+    let handle1 = thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    let handle2 = thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for r in rx {
+        println!("Got {}", r);
+    }
+
+    handle1.join().unwrap();
+    handle2.join().unwrap();
 }
